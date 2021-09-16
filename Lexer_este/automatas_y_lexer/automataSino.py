6762850efdec4da0e2565061619426a44d1ad048
Victoria Jara <63740652+JaraVictoria@users.noreply.github.com>
2021-09-16 16:08:56 -0300

    Add files via upload

---
+++ Lexer_este/automatas_y_lexer/automataSino.py
@@ -0,0 +1,31 @@
+ESTADO_FINAL = "ESTADO ACEPTADO"
+ESTADO_NO_FINAL = "ESTADO NO ACEPTADO"
+ESTADO_TRAMPA = "ESTADO TRAMPA"
+
+def automata_sino(cadena):
+	estado = 0
+	estados_finales = [4]
+
+	for caracter in cadena:
+		if estado == 0 and caracter == "s":
+			estado = 1
+		elif estado == 1 and caracter == "i":
+			estado = 2
+		elif estado == 2 and caracter == "n":
+			estado = 3
+		elif estado == 3 and caracter == "o":
+			estado = 4
+		else:
+			estado = -1
+			break
+
+	if estado == -1:
+		return ESTADO_TRAMPA
+	if estado in estados_finales:
+		return ESTADO_FINAL
+	else:
+		return ESTADO_NO_FINAL
+
+assert automata_sino("sino") == ESTADO_FINAL
+assert automata_sino("sin") == ESTADO_NO_FINAL
+assert automata_sino("siño") == ESTADO_TRAMPA
